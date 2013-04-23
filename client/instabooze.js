4cfa37884ad9920860adfb9307d781782f2f5a14
ezl <ericzliu@gmail.com>
2013-04-23 16:04:17 -0500

    oof

--- client/instabooze.js
+++ client/instabooze.js
@@ -11,6 +11,7 @@ Meteor.startup(function() {
         });
         Session.set('cart', cart);
     });
+    Session.set("step", "products");
 });
 
 var updateCart = function(itemID, delta) {
@@ -30,23 +31,42 @@ if (Meteor.isClient) {
         return _.values(Session.get("cart"));
     };
     Template.cart.events({
-         'click .add' : function () {
+         'click .order.add' : function () {
             updateCart(this.item._id, 1);
         }
     });
     Template.cart.events({
-        'click .remove' : function () {
+        'click .order.remove' : function () {
             updateCart(this.item._id, -1);
         }
     });
     Template.cart.cartItems = function() {
         return _.values(Session.get("cart"));
     };
-    Template.cart.orderTotal = function() {
+    Template.footer.orderTotal = function() {
         sum = 0;
         _.each(Session.get("cart"), function(cartItem) {
             sum += cartItem.item.price * cartItem.qty;
         });
         return sum;
     }
+Template.cart.events = {
+    'click .showCheckout': function(e, t) {
+        Session.set('step', 'checkout');
+        console.log(Session.get("step"));
+    }
+};
+ 
+Template.checkout.events = {
+    'click .showCart': function(e, t) {
+        Session.set('step', 'products');
+        console.log(Session.get("step"));
+    }
+};
+    Template.main.helpers({
+        isProductPage: function() {
+            console.log("isproductpage template helper running");
+            return Session.get("step") == "products";
+        }
+    });
 }
